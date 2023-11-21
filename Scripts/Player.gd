db7773a50cbe3864a21ee133d74353505facfc65
HonouredOne <jameswrightlpa@gmail.com>
2023-11-21 15:22:34 -0600

    added movement functionality to player script

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -3,6 +3,10 @@ extends CharacterBody2D
 @onready var animation_sprite = $AnimatedSprite2D
 
 @export var speed = 50
+var is_attacking = false
+
+var new_direction : Vector2
+var animation : String
 
 func _physics_process(delta):
 	var direction: Vector2
@@ -12,15 +16,51 @@ func _physics_process(delta):
 	if abs(direction.x) == 1 and abs(direction.y) == 1:
 		direction = direction.normalized()
 	
-	var movement = speed * direction * delta
+	if Input.is_action_pressed("ui_sprint"):
+		speed = 100
+	elif Input.is_action_just_released("ui_sprint"):
+		speed = 50
 	
-	move_and_collide(movement)
+	var movement = speed * direction * delta
+	if is_attacking == false:
+		move_and_collide(movement)
+		player_animations(direction)
+	
+	if not Input.is_anything_pressed():
+		if is_attacking == false:
+			animation = "idle_" + returned_direction(new_direction)
+
+func _input(event):
+	if event.is_action_pressed("ui_attack"):
+		is_attacking = true
+		animation = "attack_" + returned_direction(new_direction)
+		animation_sprite.play(animation)
 
 func player_animations(direction : Vector2):
 	if direction != Vector2.ZERO:
 		new_direction = direction
-		animation =
+		animation = "walk_" + returned_direction(new_direction)
 		animation_sprite.play(animation)
 	else:
-		animation = 
+		animation = "idle_" + returned_direction(new_direction)
 		animation_sprite.play(animation)
+
+func returned_direction(direction : Vector2):
+	var normalized_direction = direction.normalized()
+	var default_return = "down"
+	
+	if normalized_direction.y > 0:
+		return "down"
+	elif normalized_direction.y < 0:
+		return "up"
+	elif normalized_direction.x > 0:
+		animation_sprite.flip_h = false
+		return "side"
+	elif normalized_direction.x < 0:
+		animation_sprite.flip_h = true
+		return "side"
+	
+	return default_return
+
+func _on_animated_sprite_2d_animation_finished():
+	is_attacking = false
